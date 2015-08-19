c69a7b22063574a691240e80c93dc96164e1d367
Oleg <oleg.bezkorovajniy@gmail.com>
2015-08-19 19:08:55 +0300

    Initial commit

---
+++ src/com/xml/parser/UnknownStructureParser.java
@@ -0,0 +1,67 @@
+package com.xml.parser;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Oleg on 19 Aug 2015.
+ */
+
+public class UnknownStructureParser {
+
+    public Map<String, String> px(File file) throws Exception {
+
+        Map<String, String> map = new HashMap<>();
+        XMLStreamReader xr = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(file));
+        String name = "", value = "", attrName = "";
+
+        while (xr.hasNext()) {
+            int e = xr.next();
+            switch (e) {
+                case XMLStreamReader.START_ELEMENT: {
+                    name = xr.getLocalName();
+                    final int attributeCount = xr.getAttributeCount();
+                    if (attributeCount > 0) {
+                        attrName = xr.getAttributeName(0).getLocalPart();
+                        final String attributeValue = xr.getAttributeValue(0);
+                        System.out.println(name + " " + attrName + " " + attributeValue);
+                    }
+                    break;
+                }
+                case XMLStreamReader.CHARACTERS: {
+                    value = xr.getText();
+                    break;
+                }
+            }
+            map.put(name, value);
+        }
+        return map;
+    }
+    public Map<String, String> pz(File file) throws Exception {
+
+        Map<String, String> map = new HashMap<>();
+        XMLStreamReader xr = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(file));
+
+
+        while(xr.hasNext()) {
+
+            int e = xr.next();
+            if (e == XMLStreamReader.START_ELEMENT) {
+                String name = xr.getLocalName();
+                xr.next();
+                String value = null;
+                try {
+                    value = xr.getText();
+                } catch (IllegalStateException exep) {
+                    exep.printStackTrace();
+                }
+                map.put(name, value);
+            }
+        }
+        return map;
+    }
+}
---
+++ test/com/parser/test/ParserTest.java
@@ -0,0 +1,60 @@
+package com.parser.test;
+
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.parsers.FactoryConfigurationError;
+import java.io.File;
+import java.util.Map;
+
+import com.xml.parser.UnknownStructureParser;
+
+/**
+ * Created by Oleg on 19 Aug 2015.
+ */
+public class ParserTest {
+
+    private final static Logger logger = Logger.getLogger(ParserTest.class);
+
+    private UnknownStructureParser parser = new UnknownStructureParser();
+
+    @Test
+    public void testCurr() throws FactoryConfigurationError, Exception {
+        File f = new File("C:\\Users\\Oleg\\IdeaProjects\\XmlParser\\resources\\request.xml");
+        Map<String, String> map = parser.px(f);
+
+        // Print all Key/Value pairs
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            logger.debug("Key: " + key);
+            logger.debug("Value: " + value);
+        }
+
+        Assert.assertEquals(map.get("MonthlyPlanPremiumAmtPP"), "136");
+        Assert.assertEquals(map.get("MonthlyAdvancedPTCAmtPP"), "125");
+        Assert.assertEquals(map.get("AdjustedGrossIncomeAmt"), "22000");
+        Assert.assertEquals(map.get("TotalExemptionsCnt"), "1");
+    }
+
+    @Test
+    public void testPrev() throws FactoryConfigurationError, Exception {
+        File f = new File("C:\\Users\\Oleg\\IdeaProjects\\XmlParser\\resources\\request.xml");
+        Map<String, String> map = parser.pz(f);
+
+        // Print all Key/Value pairs
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            logger.debug("Key: " + key);
+            logger.debug("Value: " + value);
+        }
+
+        Assert.assertEquals(map.get("MonthlyPlanPremiumAmtPP"), "136");
+        Assert.assertEquals(map.get("MonthlyAdvancedPTCAmtPP"), "125");
+        Assert.assertEquals(map.get("AdjustedGrossIncomeAmt"), "22000");
+        Assert.assertEquals(map.get("TotalExemptionsCnt"), "1");
+    }
+}
